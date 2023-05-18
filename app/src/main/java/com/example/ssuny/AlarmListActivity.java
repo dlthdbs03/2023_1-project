66574a29fd25b61970f9fa4c5e4ff4b89934bf85
예림 <62548205+Yelim0529@users.noreply.github.com>
2023-05-18 22:48:09 +0900

    알람 설정(제목, 날짜, 반복횟수, 시간1개)화면 추가

--- app/src/main/java/com/example/ssuny/AlarmListActivity.java
+++ app/src/main/java/com/example/ssuny/AlarmListActivity.java
@@ -1,6 +1,9 @@
 package com.example.ssuny;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,14 @@ public class AlarmListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_list);
+
+        Button search_btn = findViewById(R.id.add_btn);
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AlarmListActivity.this, TimePickerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
